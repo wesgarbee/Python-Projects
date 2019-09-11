6832fc0afb3106b7e656e1565e5b8e66f2a7ad44
Wes Garbee <wgarbee@gmail.com>
2019-09-10 22:03:33 -0500

    added ratings and saving latest search url to txt file

--- WineWebScraper/main.py
+++ WineWebScraper/main.py
@@ -6,8 +6,9 @@ import time
 import random
 import urllib.request
 import requests
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 from bs4 import BeautifulSoup as bSoup
+from Classes.console_colors import Concolors as color
 
 
 # Establish connection
@@ -27,23 +28,22 @@ collection.create_index([("wine_id", 1)], unique=True)
 # Checks if txt file for saving page exists
 if not os.path.exists('page_progress.txt'):
     with open('page_progress.txt', 'w') as file:
-        file.write('1')
+        file.write('https://www.wine.com/list/wine/7155/1')
 
 with open('page_progress.txt', 'r') as file:
     data = file.read()
 
 # Prompts user to enter the wine.com url to scrape
-address = input("Enter wine.com URL: ").strip()
+address = data
+main_url = address.rsplit('/', 1)[0]
 # Increments the page number to scrape
-page_number = int(data)
+page_number = int(data.rsplit('/', 1)[-1])
 
 req = requests.head(address)
 code = req.status_code
 
 while code is 200:
-    page_to_pull = address + "/" + str(page_number)
-
-    req = requests.get(page_to_pull)
+    req = requests.get(address)
 
     soup = bSoup(req.text, "html.parser")
     wines = soup.findAll("li", {"class": "prodItem"})
@@ -52,7 +52,7 @@ while code is 200:
         # This checks that the wine isn't a gift set or collection
         if not wine.find("div", {"class": "prodItemInfo_origin js-is-hidden"}):
             # Gets the line of text for the name and vintage
-            wine_text = wine.find("span", {"class": "prodItemInfo_name"}).text
+            wine_text = wine.find("span", {"class": "prodItemInfo_name"}).text.replace("/", " ")
 
             # Gets the id from wine.com for later use
             wine_id = int(wine.find("meta")['content'])
@@ -72,10 +72,13 @@ while code is 200:
                 wine_name = wine_text
 
             # Gets all of the ratings for the wine
-            wine_ratings_elements = wine.find_all("li", {"class": "wineRatings_listItem"})
-            wine_ratings = []
-            for wre in wine_ratings_elements:
-                wine_ratings.append(wre['title'])
+            try:
+                wine_ratings_elements = wine.find_all("li", {"class": "wineRatings_listItem"})
+                wine_ratings = []
+                for wre in wine_ratings_elements:
+                    wine_ratings.append(wre['title'])
+            except Exception as e:
+                print("No ratings to add:", e)
 
             # Get the varietal/blend
             wine_varietal_text = wine.find("span", {"class": "prodItemInfo_varietal"}).text
@@ -90,15 +93,18 @@ while code is 200:
                         (wine_varietal_text == "Port"):
                     wine_color = wine_varietal_text
 
+            # Creates the path where images will be stored
             try:
-                # Path name where the images for this search will be stored
-                dir_name = ("./images/" + wine_color.replace(" ", "_") + "/" +
-                            wine_varietal_text.replace(" ", "_").replace("/", "_"))
+                if os.path.isdir("./images/" + wine_color.replace(" ", "_")):
+                    dir_name = ("./images/" + wine_color.replace(" ", "_") + "/" +
+                                wine_varietal_text.replace(" ", "_").replace("/", "_"))
+                else:
+                    dir_name = ("./images/Other/" + wine_varietal_text.replace(" ", "_").replace("/", "_"))
                 # If that dir does not exist, creates it.
                 if not os.path.isdir(dir_name):
                     os.mkdir(dir_name)
             except FileNotFoundError as e:
-                print(e)
+                print(color.FAIL + color.BOLD + e + color.ENDC)
 
             wine_origin = wine.find("span", {"class": "prodItemInfo_originText"}).text
             wine_img_element = wine.find('img')
@@ -116,20 +122,30 @@ while code is 200:
                         "origin": wine_origin,
                         "label_image": wine_img_path}
                 rec_id = collection.insert_one(wine).inserted_id
-                print("Data inserted with record id", rec_id)
+                print(color.OKGREEN, color.BOLD, "Data inserted with record id", rec_id, color.ENDC)
+            except KeyError as e:
+                print(color.OKBLUE, "Duplicate key: ", e.args[0], color.ENDC)
             except Exception as e:
-                print("Error: ", e)
+                print(color.FAIL, color.BOLD, "Error: ", e, color.ENDC)
         else:
             continue
 
     page_number += 1
+
+    # Writes the latest URL to the save file
     print("Page number:", page_number)
-
+    address = main_url + "/" + str(page_number)
     with open('page_progress.txt', 'w') as file:
-        file.write(str(page_number))
+        file.write(address)
 
-    # # Sets random delay in seconds so as not to overload the server
+    # Gets the latest db entry count
+    print("There are currently", collection.count(), "wines in the database.")
+
+    # Sets random delay in seconds so as not to overload the server
     seconds = random.randrange(45, 60)
+    if page_number % 3 is 0:
+        seconds += 30
+    print("Paused for", seconds, "seconds.")
     time.sleep(seconds)
 
     # Gets status code.
